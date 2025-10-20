71dff0cc19817be2b8d73b7384e96ec0ba8ed2b8
lenny288 <lennymbuthi1@gmail.com>
2025-10-20 06:45:55 -0700

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,1 +1,17 @@
+-- Create the database
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+-- Use the database
+USE alx_book_store;
+
+-- Create the Authors table
+CREATE TABLE Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+-- Create the Books table
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title V
 
